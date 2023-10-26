4ba35a0760d6b078459e3be169820f097430ee82
Tharindu <tharindunirmal996@gmail.com>
2023-10-27 02:00:32 +0530

    database connection initialized

---
+++ config/sequelize.js
@@ -0,0 +1,10 @@
+// const Sequelize = require('sequelize');
+// const config = require('./config');
+
+// console.log(config.development); // Log the configuration to the console
+
+// const sequelize = new Sequelize(config.development);
+
+// console.log(sequelize); // Log the configuration to the console
+
+// module.exports = sequelize;
--- routes/product-routes/product.routes.js
+++ routes/product-routes/product.routes.js
@@ -1,33 +1,38 @@
 const router = require('express').Router();
 const paginatedResults = require('../../middlewares/pagination');
+const db = require('../../models/index');
+const Product = require('../../models/Product')(db.sequelize);
 
-const products = [
-  { id: 1, name: 'Product 1' },
-  { id: 2, name: 'Product 2' },
-  { id: 3, name: 'Product 3' },
-  { id: 4, name: 'Product 4' },
-  { id: 5, name: 'Product 5' },
-  { id: 6, name: 'Product 6' },
-  { id: 7, name: 'Product 7' },
-  { id: 8, name: 'Product 8' },
-  { id: 9, name: 'Product 9' },
-  { id: 10, name: 'Product 10' },
-  { id: 11, name: 'Product 11' },
-];
+// router.route('/items').get(async (req, res) => {
+//   try {
+//     const products = await Product.findAll();
+//     res.json(products);
+//   } catch (error) {
+//     console.error('Error fetching products:', error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// });
 
-router.route('/items').get(paginatedResults(products), (req, res) => {
+router.route('/items').get(paginatedResults(Product), (req, res) => {
   res.json(res.paginatedResults);
 });
 
-router.route('/item/:id').get((req, res) => {
+router.route('/item/:id').get(async (req, res) => {
   const itemId = parseInt(req.params.id);
 
-  const foundItem = products.find((item) => item.id === itemId);
+  console.log(Product);
 
-  if (foundItem) {
-    res.json(foundItem);
-  } else {
-    res.status(404).json({ message: 'Item not found' });
+  try {
+    const foundItem = await Product.findByPk(itemId);
+
+    if (foundItem) {
+      res.json(foundItem);
+    } else {
+      res.status(404).json({ message: 'Item not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching product:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
